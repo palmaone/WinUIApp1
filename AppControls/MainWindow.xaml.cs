3d69c8b89b1ab94cef0a9b69a5559fffb2675bf3
Israel Palma <isra.palma@gmail.com>
2022-08-19 13:30:53 -0500

    Add project files.

---
+++ AppControls/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace AppControls
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
